462da5dfba6801f832359fe5e4cdb439529f3823
UFO <gmorales@utexas.edu>
2017-08-09 02:18:07 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -41,30 +41,39 @@ var createBoard = function() {
 var reset = function() {
     document.getElementById('game-board').innerHTML = "";
     createBoard();
+    score = 0;
+    updateScore();
 };
 
 document.getElementById('reset-button').addEventListener('click', reset);
 
 var checkForMatch = function() {
-
-    if(cardsInPlay.length === 2) {
-        if(cardsInPlay[0] === cardsInPlay[1]) {
-            alert("You found a match!");
-            score += 1;
-            updateScore();
-            cardsInPlay = [];
-        } else {
-            alert("Sorry, try again.");
-            cardsInPlay = [];
-            reset();
+    if(score === (cards.length / 2)) {
+        alert("You already found all the matches!");
+    } else {
+        if(cardsInPlay.length === 2) {
+            if(cardsInPlay[0] === cardsInPlay[1]) {
+                alert("You found a match!");
+                score += 1;
+                updateScore();
+                cardsInPlay = [];
+            } else {
+                alert("Sorry, try again.");
+                cardsInPlay = [];
+                reset();
+            }
         }
+
     }
 };
 
 var flipCard = function() {
     var cardId = this.getAttribute('data-id');
     this.setAttribute('src', cards[cardId].cardImage);
-    cardsInPlay.push(cards[cardId].rank);
+    if(score < (cards.length / 2)) {
+        cardsInPlay.push(cards[cardId].rank);
+    }
+
     checkForMatch();
 
 
