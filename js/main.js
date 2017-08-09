10ae53db80487c17c3257c4306f4766bcd94403f
UFO <gmorales@utexas.edu>
2017-08-09 00:33:19 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -22,25 +22,52 @@ var cards = [
 
 ];
 var cardsInPlay = [];
+var score = 0;
+
+var updateScore = function() {
+    document.getElementById('score').innerHTML = score;
+}
+
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+}
+
+var reset = function() {
+    document.getElementById('game-board').innerHTML = "";
+    createBoard();
+};
+
+document.getElementById('reset-button').addEventListener('click', reset);
 
 var checkForMatch = function() {
+
     if(cardsInPlay.length === 2) {
         if(cardsInPlay[0] === cardsInPlay[1]) {
             alert("You found a match!");
+            score += 1;
+            updateScore();
+            cardsInPlay = [];
         } else {
             alert("Sorry, try again.");
+            cardsInPlay = [];
+            reset();
         }
     }
 };
 
-var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+    var cardId = this.getAttribute('data-id');
+    this.setAttribute('src', cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
-    console.log(cards[cardId].cardImage);
-    console.log(cards[cardId].suit);
-
     checkForMatch();
+
+
 };
 
-flipCard(0);
-flipCard(2);
+createBoard();
